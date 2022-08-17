10d2235450d1cd24050075e3de38b10720717e40
zhukavets <stewart-8@mail.ru>
2022-08-17 22:49:13 +0300

    Find the Duplicate Number

---
+++ 15.FindTheDuplicateNumber.js
@@ -0,0 +1,28 @@
+//Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+//
+// There is only one repeated number in nums, return this repeated number.
+//
+// You must solve the problem without modifying the array nums and uses only constant extra space.
+//
+//
+//
+// Example 1:
+//
+// Input: nums = [1,3,4,2,2]
+// Output: 2
+
+let nums = [3, 1, 3, 4, 2]
+
+let findDuplicate = function (nums) {
+    let SortedArr = nums.sort((a, b) => a - b)
+    let current = NaN
+    for (let num of SortedArr) {
+        if (current === num) {
+            return current
+        } else {
+            current = num
+        }
+    }
+};
+
+console.log(findDuplicate(nums))
